e3164155ac4477c15806c40d548c40ebb1b1077f
Nikita Hismatov <hismatovns@gmail.com>
2012-07-09 11:23:55 +0600

    меню django-cms

---
+++ menu.py
@@ -0,0 +1,21 @@
+#-*- coding: UTF-8 -*-
+from cms.menu_bases import CMSAttachMenu
+from menus.base import NavigationNode
+from menus.menu_pool import menu_pool
+
+from .models import {{app_name|capfirst}}Item
+
+class {{app_name|capfirst}}Menu(CMSAttachMenu):
+    name = u'{{app_name}} menu'
+
+    def get_nodes(self, request):
+        nodes = []
+
+        for item in {{app_name|capfirst}}Item.objects.all():
+            nodes.append(NavigationNode(item.address,
+                                        item.get_absolute_url(),
+                                        item.pk))
+
+        return nodes
+
+menu_pool.register_menu(ResellMenu) # register the menu
